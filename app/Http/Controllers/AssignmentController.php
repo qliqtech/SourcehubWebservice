5136871bd615bd04011a1dee7132a27fae16766e
David <davidkumi10>
2021-11-13 18:24:40 +0000

    Added Creation of Assigments, Submitting and marking

---
+++ app/Http/Controllers/AssignmentController.php
@@ -0,0 +1,250 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\ApiResponseCodesKeysAndMessages;
+use App\Enums\InAppResponsTypes;
+use App\ImplementationService\AssignmentService;
+use App\ImplementationService\AuthenticationService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+class AssignmentController extends Controller
+{
+
+
+
+
+    public function createassignment (Request $request) {
+
+        $request->request->add($this->GetUserAgent($request));
+
+
+
+        $validator = Validator::make($request->all(), [
+
+            'title' => 'required|string',
+            'task' => 'required|string',
+            'classid' => 'required|string',
+        ]);
+        if ($validator->fails())
+        {
+
+            $responsevalues = array(ApiResponseCodesKeysAndMessages::ResponseCodeKey=>ApiResponseCodesKeysAndMessages::SuccessCode,
+                ApiResponseCodesKeysAndMessages::ResponseMessageCodeKey=>'Validation Errors',
+                'errors'=>$validator->errors()->all()
+
+
+            );
+
+
+            return response($responsevalues);
+
+
+        }
+        $allkeys = $request->all();
+
+
+        $service = new AssignmentService();
+
+        $response = $service->createassignment($allkeys);
+
+        //  dd($response);
+
+        if($response[InAppResponsTypes::responsetypekey] == InAppResponsTypes::Success){
+
+
+            $responsevalues = array(ApiResponseCodesKeysAndMessages::ResponseCodeKey=>ApiResponseCodesKeysAndMessages::SuccessCode,
+                ApiResponseCodesKeysAndMessages::ResponseMessageCodeKey=>'Assignment Created',
+
+
+
+            );
+
+
+            return response($responsevalues);
+        }
+
+
+        $responsevalues = array(ApiResponseCodesKeysAndMessages::ResponseCodeKey=>ApiResponseCodesKeysAndMessages::FailedCode,
+            ApiResponseCodesKeysAndMessages::ResponseMessageCodeKey=>$response[InAppResponsTypes::responsemessagekey],
+
+
+
+        );
+
+
+        return response($responsevalues);
+
+
+    }
+
+
+
+
+
+    public function markassignmentandcomment (Request $request) {
+
+        $request->request->add($this->GetUserAgent($request));
+
+
+
+        $allkeys = $request->all();
+
+
+        $service = new AssignmentService();
+
+        $response = $service->markassignmentandcomment($allkeys);
+
+        //  dd($response);
+
+        if($response[InAppResponsTypes::responsetypekey] == InAppResponsTypes::Success){
+
+
+            $responsevalues = array(ApiResponseCodesKeysAndMessages::ResponseCodeKey=>ApiResponseCodesKeysAndMessages::SuccessCode,
+                ApiResponseCodesKeysAndMessages::ResponseMessageCodeKey=>'Assignment Marked',
+
+
+
+            );
+
+
+            return response($responsevalues);
+        }
+
+
+        $responsevalues = array(ApiResponseCodesKeysAndMessages::ResponseCodeKey=>ApiResponseCodesKeysAndMessages::FailedCode,
+            ApiResponseCodesKeysAndMessages::ResponseMessageCodeKey=>$response[InAppResponsTypes::responsemessagekey],
+
+
+
+        );
+
+
+        return response($responsevalues);
+
+
+    }
+
+
+
+
+
+
+    public function submitassignment (Request $request) {
+
+        $request->request->add($this->GetUserAgent($request));
+
+
+
+        //attempt fileupload on AWS and provideurl
+
+      if(strtolower($request->file('fileupload')->getClientOriginalExtension()) != 'zip'){
+
+
+          echo "Upload Zip Files Only";die();
+
+      }
+
+
+        $filename = $request->file('fileupload')->getClientOriginalName();
+
+
+
+        $storagePath = Storage::disk('s3')->putFileAs('assignments/'.$request->user()->email,$request->file('fileupload'),$filename ,'public');
+
+        $bucketbasepath = env('AWS_BASEURLSOURCEHUB', 'aaa');
+
+
+        $request->request->add(array('answerurl'=> $bucketbasepath.$storagePath));
+
+
+        $allkeys = $request->all();
+
+
+        $service = new AssignmentService();
+
+
+
+
+        $response = $service->submitassignment($allkeys);
+
+        //  dd($response);
+
+        if($response[InAppResponsTypes::responsetypekey] == InAppResponsTypes::Success){
+
+
+            $responsevalues = array(ApiResponseCodesKeysAndMessages::ResponseCodeKey=>ApiResponseCodesKeysAndMessages::SuccessCode,
+                ApiResponseCodesKeysAndMessages::ResponseMessageCodeKey=>'Assignment Submitted',
+
+            );
+
+
+            return response($responsevalues);
+        }
+
+
+        $responsevalues = array(ApiResponseCodesKeysAndMessages::ResponseCodeKey=>ApiResponseCodesKeysAndMessages::FailedCode,
+            ApiResponseCodesKeysAndMessages::ResponseMessageCodeKey=>$response[InAppResponsTypes::responsemessagekey],
+
+
+
+        );
+
+
+        return response($responsevalues);
+
+
+    }
+
+
+
+
+
+    public function listassigments (Request $request) {
+
+        $request->request->add($this->GetUserAgent($request));
+
+
+
+        $allkeys = $request->all();
+
+
+        $service = new AssignmentService();
+
+        $response = $service->createassignment($allkeys);
+
+        //  dd($response);
+
+        if($response[InAppResponsTypes::responsetypekey] == InAppResponsTypes::Success){
+
+
+            $responsevalues = array(ApiResponseCodesKeysAndMessages::ResponseCodeKey=>ApiResponseCodesKeysAndMessages::SuccessCode,
+                ApiResponseCodesKeysAndMessages::ResponseMessageCodeKey=>'Assignment Created',
+
+
+
+            );
+
+
+            return response($responsevalues);
+        }
+
+
+        $responsevalues = array(ApiResponseCodesKeysAndMessages::ResponseCodeKey=>ApiResponseCodesKeysAndMessages::FailedCode,
+            ApiResponseCodesKeysAndMessages::ResponseMessageCodeKey=>$response[InAppResponsTypes::responsemessagekey],
+
+
+
+        );
+
+
+        return response($responsevalues);
+
+
+    }
+
+
+
+}
---
+++ app/ImplementationService/AssignmentService.php
@@ -0,0 +1,332 @@
+<?php
+
+namespace App\ImplementationService;
+
+use App\DBOperations\AssignmentDBOperations;
+use App\DBOperations\UserDBOperations;
+use App\Enums\InAppResponsTypes;
+use App\Helpers\TimeHelper;
+use App\Models\Assignment;
+use App\Models\Assignment_students;
+use App\Models\User;
+use Illuminate\Support\Facades\Storage;
+
+class AssignmentService extends BaseImplemetationService
+{
+
+
+    public function createassignment($params) : array
+    {
+
+
+        $assignment = new Assignment();
+
+        $user = new User();
+
+
+        $assignmentDBOperations = new AssignmentDBOperations($assignment);
+
+        $useroperations = new UserDBOperations($assignment);
+
+
+        $responsearray = array();
+
+
+        try {
+
+            if ($params == null) {
+
+
+                return $this::responseHelper($responsearray)[0];
+            }
+
+            $params['activityname'] = "Assignment created";
+
+          //  $params['responsemessage'] = $ex->getMessage();
+
+            $assignmentcreated = $assignmentDBOperations->create($params);
+
+
+
+            $classid = $params['classid'];
+
+
+
+            $listofstudentsinclass = $useroperations->listall()->where('classid','=',$classid);
+
+         //   dd($listofstudentsinclass);
+
+            $this::matchassignmenttostudent($listofstudentsinclass->toArray(),$assignmentcreated->id);
+
+            $responsearray = array(InAppResponsTypes::responsetypekey => InAppResponsTypes::Success,
+                InAppResponsTypes::responsemessagekey => "Assignment created successfully"
+            );
+
+            //send bulk email to class
+
+
+
+
+
+        }catch (\Exception $ex){
+
+
+            $responsearray = array(InAppResponsTypes::responsetypekey => InAppResponsTypes::Error,
+                InAppResponsTypes::responsemessagekey => $ex->getMessage()
+            );
+
+            $params['activityname'] = "Assignment created";
+
+            $params['responsemessage'] = $ex->getMessage();
+
+            //    return $this::responseHelper($responsearray)[0];
+        }
+
+        $responsearray = array_add($responsearray,'AuditItems',$params);
+
+
+        //  dd($responsearray);
+
+
+
+        return $this::responseHelper($responsearray)[0];
+    }
+
+
+    private function matchassignmenttostudent($studentlistarray,$assignmentid){
+
+        $assignment = new Assignment();
+
+        $assignmentdboperations = new AssignmentDBOperations($assignment);
+
+        foreach ($studentlistarray as $student){
+
+
+            $student = array_add($student,'assignmentid',$assignmentid);
+
+            $student = array_add($student,'userid',$student['id']);
+
+
+            //   dd($student);
+
+            $assignmentdboperations->createstudentandassignment($student);
+
+
+        }
+
+    }
+
+
+    public function listassignments($params) : array
+    {
+
+
+        $assignment = new Assignment();
+
+        $assignmentDBOperations = new AssignmentDBOperations($assignment);
+
+        $responsearray = array();
+
+
+        try {
+
+            if ($params == null) {
+
+
+                return $this::responseHelper($responsearray)[0];
+            }
+
+            $params['activityname'] = "Assignment List";
+
+            //  $params['responsemessage'] = $ex->getMessage();
+
+            $assignmentlist = $assignmentDBOperations->listall();
+            //  $token = $assignment->createToken('creds')->accessToken;
+
+            $responsearray = array(InAppResponsTypes::responsetypekey => InAppResponsTypes::Success,
+                InAppResponsTypes::responsemessagekey => "Assignment listed successfully",
+             //   ''=>''
+            );
+
+
+
+
+        }catch (\Exception $ex){
+
+
+            $responsearray = array(InAppResponsTypes::responsetypekey => InAppResponsTypes::Error,
+                InAppResponsTypes::responsemessagekey => $ex->getMessage()
+            );
+
+            $params['activityname'] = "Assignment Listed";
+
+            $params['responsemessage'] = $ex->getMessage();
+
+            //    return $this::responseHelper($responsearray)[0];
+        }
+
+        $responsearray = array_add($responsearray,'AuditItems',$params);
+
+
+        //  dd($responsearray);
+
+
+
+        return $this::responseHelper($responsearray)[0];
+    }
+
+
+
+
+    public function markassignmentandcomment($params) : array
+    {
+
+
+        $assignmentstudents = new Assignment_students();
+
+
+        $userassignmentid = $params['userassignmentid'];
+
+
+        $assignmentDBOperations = new AssignmentDBOperations($assignmentstudents);
+
+
+
+
+        $responsearray = array();
+
+
+        try {
+
+            if ($params == null) {
+
+
+                return $this::responseHelper($responsearray)[0];
+            }
+
+            $params['activityname'] = "Assignment Marked";
+
+            $params['markedon'] = \App\Helper\TimeHelper::getCurrentDateTime();
+
+            $params['markedby'] = $params['userid'];
+
+            $params['ismarked'] = true;
+
+
+            // dd($params);
+
+
+
+            $assignmentDBOperations->update(intval($userassignmentid),$params);
+
+
+            $responsearray = array(InAppResponsTypes::responsetypekey => InAppResponsTypes::Success,
+                InAppResponsTypes::responsemessagekey => "Assignment Marked successfully"
+            );
+
+
+
+        }catch (\Exception $ex){
+
+
+            $responsearray = array(InAppResponsTypes::responsetypekey => InAppResponsTypes::Error,
+                InAppResponsTypes::responsemessagekey => $ex->getMessage()
+            );
+
+            $params['activityname'] = "Assignment created";
+
+            $params['responsemessage'] = $ex->getMessage();
+
+            //    return $this::responseHelper($responsearray)[0];
+        }
+
+        $responsearray = array_add($responsearray,'AuditItems',$params);
+
+
+        //  dd($responsearray);
+
+
+
+        return $this::responseHelper($responsearray)[0];
+    }
+
+
+
+
+    public function submitassignment($params) : array
+    {
+
+
+        $assignmentstudents = new Assignment_students();
+
+
+        $userassignmentid = $params['userassignmentid'];
+
+
+        $assignmentDBOperations = new AssignmentDBOperations($assignmentstudents);
+
+
+
+        $responsearray = array();
+
+
+        try {
+
+            if ($params == null) {
+
+
+                return $this::responseHelper($responsearray)[0];
+            }
+
+            $params['activityname'] = "Assignment Submitted";
+
+            $params['issubmitted'] = true;
+
+            $params['submittedon'] = \App\Helper\TimeHelper::getCurrentDateTime();;
+
+
+
+
+            $assignmentDBOperations->update(intval($userassignmentid),$params);
+
+
+            $responsearray = array(InAppResponsTypes::responsetypekey => InAppResponsTypes::Success,
+                InAppResponsTypes::responsemessagekey => "Assignment Marked successfully"
+            );
+
+
+       //     $storagePath = Storage::disk('s3')->putFileAs($base_location,$request->file('fileupload'),$imageName ,'public');
+
+         //   $objecturl = "https://inpath-logistics-hub-bucket.s3-eu-west-1.amazonaws.com/".$storagePath;
+
+
+
+
+        }catch (\Exception $ex){
+
+
+            $responsearray = array(InAppResponsTypes::responsetypekey => InAppResponsTypes::Error,
+                InAppResponsTypes::responsemessagekey => $ex->getMessage()
+            );
+
+            $params['activityname'] = "Assignment created";
+
+            $params['responsemessage'] = $ex->getMessage();
+
+            //    return $this::responseHelper($responsearray)[0];
+        }
+
+        $responsearray = array_add($responsearray,'AuditItems',$params);
+
+
+        //  dd($responsearray);
+
+
+
+        return $this::responseHelper($responsearray)[0];
+    }
+
+
+
+
+}
